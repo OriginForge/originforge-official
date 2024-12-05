32b3500fa13b2f505cc1cb8c66beb9e32ceb0872
jangju jo <team.gutalik@gmail.com>
2024-12-05 18:48:20 +0900

    .

--- src/components/TelegramLogin.jsx
+++ src/components/TelegramLogin.jsx
@@ -4,6 +4,11 @@ export const TelegramLogin = () => {
     return <LoginButton 
     // testbot
         botUsername={"7355830225:AAELimJfV7OhRSVEgHdzfR0SZUZDizBniBs"}
+        authCallbackUrl="/path/to/callback/url"
+                buttonSize="large" // "large" | "medium" | "small"
+                cornerRadius={5} // 0 - 20
+                showAvatar={true} // true | false
+                lang="en"
         onAuthCallback={(data)=> {
             console.log(data);
             axios.post(`https://api.origin-forge.com/auth/telegram`, {
